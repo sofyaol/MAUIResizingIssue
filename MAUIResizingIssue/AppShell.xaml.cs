3cad2fe837d4e094057fcafb504bdd32d9e17d84
sofyaol <sofyaol@hotmail.com>
2022-07-07 13:49:07 +0300

    resizing issue

---
+++ MAUIResizingIssue/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIResizingIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUIResizingIssue/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MAUIResizingIssue;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+     void OnButtonClicked(object sender, EventArgs args)
+    {
+		mainLabel.Text += mainLabel.Text;
+    }
+
+}
+
